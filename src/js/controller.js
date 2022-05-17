b9869bdf3506b6b8f37c2a8964b776d07d8e1dd3
Omar <omared250@gmail.com>
2022-05-17 14:21:57 +0200

    adding dependecis as runtime and core-js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 // import icons from '../img/icons.svg'; // Parce 1
 import icons from 'url:../img/icons.svg'; // Parce 2
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -15,9 +17,21 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function(parentEl) {
+  const markup = `
+  <div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>`
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 const showRecipe = async function() {
   try {
     // 1). Loading recepi
+    renderSpinner(recipeContainer);
     const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
     // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8fd');
     const data = await res.json();
