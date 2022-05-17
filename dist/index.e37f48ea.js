70d9c6aed450c2cf54af19c8cef3dd3bd6334b3c
Omar <omared250@gmail.com>
2022-05-17 13:56:19 +0200

    rendering the recipe

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -539,15 +539,15 @@ const timeout = function(s) {
 const showRecipe = async function() {
     try {
         // 1). Loading recepi
-        // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8fd');
+        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8fd');
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} $`);
         let { recipe  } = data.data;
         recipe = {
             id: recipe.id,
             title: recipe.title,
-            pusblisher: recipe.pusblisher,
+            publisher: recipe.publisher,
             sourceUrl: recipe.source_url,
             image: recipe.image_url,
             servings: recipe.servings,
@@ -557,25 +557,10 @@ const showRecipe = async function() {
         console.log(recipe);
         //2). Rendering Recepi
         const markup = `
-    <div class="spinner">
-      <svg>
-        <use href="src/img/icons.svg#icon-loader"></use>
-      </svg>
-    </div>
-
-    <div class="error">
-      <div>
-        <svg>
-          <use href="src/img/icons.svg#icon-alert-triangle"></use>
-        </svg>
-      </div>
-          <p>No recipes found for your query. Please try again!</p>
-    </div>
-
     <figure class="recipe__fig">
-      <img src="src/img/test-1.jpg" alt="Tomato" class="recipe__img" />
+      <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
         <h1 class="recipe__title">
-          <span>Pasta with tomato cream sauce</span>
+          <span>${recipe.title}</span>
         </h1>
     </figure>
 
@@ -584,14 +569,14 @@ const showRecipe = async function() {
         <svg class="recipe__info-icon">
           <use href="src/img/icons.svg#icon-clock"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--minutes">45</span>
+        <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookinTime}</span>
         <span class="recipe__info-text">minutes</span>
       </div>
       <div class="recipe__info">
         <svg class="recipe__info-icon">
           <use href="src/img/icons.svg#icon-users"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--people">4</span>
+        <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
         <span class="recipe__info-text">servings</span>
 
         <div class="recipe__info-buttons">
@@ -623,27 +608,19 @@ const showRecipe = async function() {
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-        <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="src/img/icons.svg#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">1000</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">g</span>
-              pasta
-          </div>
-        </li>
-
-        <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="src/img/icons.svg#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">0.5</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">cup</span>
-              ricotta cheese
-          </div>
-        </li>
+        ${recipe.ingredients.map((ing)=>{
+            return `
+          <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="src/img/icons.svg#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${ing.quantity}</div>
+            <div class="recipe__description">
+              <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+            </div>
+          </li>`;
+        }).join('')}
       </ul>
     </div>
 
@@ -651,12 +628,12 @@ const showRecipe = async function() {
       <h2 class="heading--2">How to cook it</h2>
       <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
-        <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+        <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
           directions at their website.
       </p>
       <a
         class="btn--small recipe__btn"
-        href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+        href="${recipe.sourceUrl}"
         target="_blank"
       >
         <span>Directions</span>
@@ -665,6 +642,8 @@ const showRecipe = async function() {
           </svg>
       </a>
     </div>`;
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML('afterbegin', markup);
     } catch (err) {
         alert(err);
     }
