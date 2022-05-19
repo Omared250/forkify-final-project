9c631d59f25367161ac0005b733c936d66bacbc9
Omar <omared250@gmail.com>
2022-05-19 15:29:58 +0200

    refactoring recipes and search code

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,10 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function() {
   try {
     const id = window.location.hash.slice(1);
