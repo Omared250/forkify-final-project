6fa62e7746f77b30874cd7bd884ab140c50f2b16
Omar <omared250@gmail.com>
2022-05-28 20:28:00 +0200

    make functional the open, close and overlay buttons

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3093,12 +3093,18 @@ class addRecipeView extends _viewJsDefault.default {
     constructor(){
         super();
         this._addHandlerShowWindow();
+        this._addHandlerHideWindow();
+    }
+    toggleWindow() {
+        this._overlay.classList.toggle('hidden');
+        this._window.classList.toggle('hidden');
     }
     _addHandlerShowWindow() {
-        this._btnOpen.addEventListener('click', function() {
-            this._overlay.classList.toggle('hidden');
-            this._window.classList.toggle('hidden');
-        });
+        this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+    }
+    _addHandlerHideWindow() {
+        this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+        this._overlay.addEventListener('click', this.toggleWindow.bind(this));
     }
     _generateMarkup() {}
 }
