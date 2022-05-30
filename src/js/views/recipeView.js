7ad800a9c9e57cbcd6fc5a026cbbb2ac36e997ac
Omar <omared250@gmail.com>
2022-05-30 21:35:45 +0200

    changing the fraction function

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './view.js';
 
 // import icons from '../img/icons.svg'; // Parce 1
 import icons from 'url:../../img/icons.svg'; // Parce 2
-import { Fraction } from 'fractional';
+import fracty from "fracty";
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -112,7 +112,7 @@ class RecipeView extends View {
           <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
           </svg>
-          <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+          <div class="recipe__quantity">${ing.quantity ? new fracty(ing.quantity).toString() : ''}</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
               ${ing.description}
